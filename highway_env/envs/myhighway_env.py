8cb22d7831d2f9d42414e95bbdc2bd44d66f5347
tanty04 <1193488418@qq.com>
2023-09-03 18:21:36 +0800

    New environment created.Existing files adapted.

---
+++ highway_env/envs/myhighway_env.py
@@ -0,0 +1,125 @@
+from typing import Dict, Text
+
+import numpy as np
+
+from highway_env import utils
+from highway_env.envs.common.abstract import AbstractEnv
+from highway_env.envs.common.action import Action
+from highway_env.road.road import Road, RoadNetwork
+from highway_env.utils import near_split
+from highway_env.vehicle.controller import ControlledVehicle
+from highway_env.vehicle.kinematics import Vehicle
+
+Observation = np.ndarray
+
+class MyHighWay(AbstractEnv):
+    """
+    A highway driving environment.
+
+    A variant of highway_env.Vehicles use discrete action space.Obstacles added on the road.Possibility of other vehicles
+    suddenly deceleration or stopping.
+    """
+
+    def default_config(cls) -> dict:
+        config = super().default_config()
+        config.update({
+            "observation": {
+                "type": "Kinematics"
+            },
+            "action": {
+                "type": "DiscreteAction"
+            },
+            "lanes_count": 4,
+            "vehicles_count": 50,
+            "controlled_vehicles": 1,
+            "initial_lane_id": None,
+            "duration": 500,  # [s]
+            "ego_spacing": 2,  # Ideal spacing with the front vehicle
+            "vehicles_density": 1,
+            "collision_reward": -1,  # The reward received when colliding with a vehicle.
+            "right_lane_reward": 0.1,  # The reward received when driving on the right-most lanes, linearly mapped to
+                                       # zero for other lanes.
+            "high_speed_reward": 0.4,  # The reward received when driving at full speed, linearly mapped to zero for
+                                       # lower speeds according to config["reward_speed_range"].
+            "lane_change_reward": 0,  # The reward received at each lane change action.
+            "reward_speed_range": [20, 30],
+            "normalize_reward": True,
+            "offroad_terminal": False,
+            "random_obstacle": False,
+            "sudden_brake": False,
+        })
+        return config
+
+    def _reset(self) -> None:
+        self._create_road()
+        self._create_vehicles()
+
+
+    def _create_road(self):
+        """Create a road composed of straight adjacent lanes."""
+        self.road = Road(network=RoadNetwork.straight_road_network(self.config["lanes_count"], speed_limit=33.33),
+                         np_random=self.np_random, record_history=self.config["show_trajectories"])
+
+    def _create_vehicles(self) -> None:
+        """Create some new random vehicles of a given type, and add them on the road."""
+        other_vehicles_type = utils.class_from_path(self.config["other_vehicles_type"])
+        other_per_controlled = near_split(self.config["vehicles_count"], num_bins=self.config["controlled_vehicles"])
+
+        self.controlled_vehicles = []
+        for others in other_per_controlled:
+            vehicle = Vehicle.create_random(
+                self.road,
+                speed=30.56,
+                lane_id=self.config["initial_lane_id"],
+                spacing=self.config["ego_spacing"]
+            )
+            vehicle = self.action_type.vehicle_class(self.road, vehicle.position, vehicle.heading, vehicle.speed)
+            self.controlled_vehicles.append(vehicle)
+            self.road.vehicles.append(vehicle)
+
+            for _ in range(others):
+                vehicle = other_vehicles_type.create_random(self.road, spacing=1 / self.config["vehicles_density"])
+                vehicle.randomize_behavior()
+                self.road.vehicles.append(vehicle)
+    def _reward(self, action: Action) -> float:
+        """
+        The reward is written to foster driving at a relative high speed(but within speed limit),keeping a safe spacing
+        with its front vehicle and staying on the road for as long as it could
+        :param action: the last action performed
+        :return: the reward it gets
+        """
+        rewards = self._rewards(action)
+
+    def _rewards(self, action: Action) -> Dict[Text, float]:
+        neighbours = self.road.network.all_side_lanes(self.vehicle.lane_index)
+        lane = self.vehicle.target_lane_index[2] if isinstance(self.vehicle, ControlledVehicle) \
+            else self.vehicle.lane_index[2]
+        # Use forward speed rather than speed, see https://github.com/eleurent/highway-env/issues/268
+        forward_speed = self.vehicle.speed * np.cos(self.vehicle.heading)
+        scaled_speed = utils.lmap(forward_speed, self.config["reward_speed_range"], [0, 1])
+        # Foster agents to keep a safe spacing with its front vehicle
+        v = self.vehicle.speed
+        if v >= 27.78:
+            safe_spacing = 5
+        elif v >= 16.67:
+            safe_spacing = v * 0.18
+        elif 0 <= v < 16.67:
+            safe_spacing = 2.5
+
+        return {
+            "safe_spacing_reward": 0.9 * int(self.vehicle.ego_spacing >= safe_spacing),
+            "collision_reward": float(self.vehicle.crashed),
+            "right_lane_reward": lane / max(len(neighbours) - 1, 1),
+            "high_speed_reward": np.clip(scaled_speed, 0, 1),
+            "on_road_reward": float(self.vehicle.on_road)
+        }
+
+    def _is_terminated(self) -> bool:
+        """The episode is over if the ego vehicle crashed."""
+        return (self.vehicle.crashed or
+                self.config["offroad_terminal"] and not self.vehicle.on_road)
+
+    def _is_truncated(self) -> bool:
+        """The episode is truncated if the time limit is reached."""
+        return self.time >= self.config["duration"]
+
